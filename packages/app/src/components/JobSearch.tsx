a7bbc670ee956dd8b0867b4995ae92cb029f2f81
mandyxuuu <minliang.xu88@gmail.com>
2025-11-30 15:01:09 -0500

    change colors button for popup message

--- packages/app/src/components/JobSearch.tsx
+++ packages/app/src/components/JobSearch.tsx
@@ -561,7 +561,7 @@ const JobSearch: React.FC = () => {
                       >
                         OK
                       </button>
-                      <button className='px-4 py-2 border border-gray-300 dark:border-gray-700 rounded-md text-gray-700 dark:text-gray-200' onClick={() => setShowConfirm(false)}>Cancel</button>
+                      <button className='px-4 py-2 text-gray-700 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 hover:bg-gray-200 dark:hover:bg-gray-600 hover:text-red-600 dark:hover:text-red-400 rounded-md font-medium transition-colors' onClick={() => setShowConfirm(false)}>Cancel</button>
                     </div>
                   </div>
                 </div>
